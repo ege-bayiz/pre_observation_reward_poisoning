14398ffa19c39712ca537dcf5f36570a97eb06a9
ege-bayiz <egebayiz@utexas.edu>
2022-05-07 21:51:29 -0500

    cleaned up test code

--- attacker.py
+++ attacker.py
@@ -5,6 +5,7 @@ import victims
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 import numpy as np
+import palette
 from abc import ABC, abstractmethod
 from stable_baselines3.common.vec_env import DummyVecEnv
 from stable_baselines3 import PPO
@@ -39,20 +40,21 @@ class ZeroAttack(AttackModel):
 
 class RandomAttack(AttackModel):
     def __init__(self, victim: victims.BanditVictim):
+        np.random.seed(1)
         self.victim = victim
 
     def victim(self):
         return self.victim
 
     def predict(self, obs):
-        return np.zeros(self.victim.action_space.shape), 0
+        return self.victim.action_space.sample(), 0
 
 
 def test_attacker(model, victim):
     np.random.seed(1)
 
     num_iter = 10000
-    num_tests = 10
+    num_tests = 30
 
     obs = victim.reset()
     avg_reward = np.zeros(num_iter)
@@ -77,7 +79,6 @@ def test_attacker(model, victim):
     return avg_reward, avg_cum_regret, attacker_reward
 
 
-
 def main():
     environment = env.generate_10_arm_testbed('StandardNormal')
     bandit = alg.Exp3Algorithm(10, 0.001)
@@ -93,72 +94,68 @@ def main():
     vanilla_model = ZeroAttack(victim)
     vanilla_victim_reward, vanilla_victim_cum_regret, vanilla_attacker_reward = test_attacker(vanilla_model, victim)
 
-    plt.plot(vanilla_victim_cum_regret)
-    plt.show()
-    plt.plot(vanilla_attacker_reward)
-    plt.show()
+    ## Uniform random attack
+    random_attack_model = RandomAttack(victim)
+    random_attack_victim_reward, random_attack_victim_cum_regret, random_attack_attacker_reward = test_attacker(random_attack_model, victim)
 
-    # PPO_model = PPO('MlpPolicy', victim, verbose=1, n_epochs=1000)
-    # PPO_model.learn(total_timesteps=10000)
-    #
-    # PPO_model.save("models/ppo_all_arms_policy")
-    #
-    # PPO_model = PPO.load("models/ppo_all_arms_policy")
+    ## PPO
+    PPO_model = PPO('MlpPolicy',
+                    victim,
+                    verbose=1)
+    PPO_model.learn(total_timesteps=1000000)
+    PPO_model.save("models/ppo_chosen_arm_action_reward")
+    PPO_model = PPO.load("models/ppo_chosen_arm_action_reward", victim)
+    ppo_victim_reward, ppo_victim_cum_regret, ppo_attacker_reward = test_attacker(PPO_model, victim)
 
-    # SAC_model = SAC.load("models/sac_all_arms_action_reward3", victim)
-    #
-    # # SAC_model = SAC('MlpPolicy', victim,
-    # #                 verbose=1,
-    # #                 ent_coef='auto_0.1')
-    # SAC_model.learn(total_timesteps=100000, log_interval=4)
-    #
-    # SAC_model.save("models/sac_all_arms_action_reward3")
-    #
-    # sac_victim_reward, sac_victim_cum_regret, sac_attacker_reward = test_attacker(SAC_model, victim)
-    #
-    # plt.plot(sac_victim_cum_regret)
-    # plt.show()
-    # plt.plot(sac_attacker_reward)
-    # plt.show()
+    ## SAC
+    SAC_model = SAC('MlpPolicy',
+                    victim,
+                    verbose=1,
+                    ent_coef='auto_0.1')
+    SAC_model.learn(total_timesteps=1000000,
+                    log_interval=100)
+    SAC_model.save("models/sac_chosen_arm_action_reward")
+    SAC_model = SAC.load("models/sac_chosen_arm_action_reward", victim)
+    sac_victim_reward, sac_victim_cum_regret, sac_attacker_reward = test_attacker(SAC_model, victim)
 
-    # A2C_model = A2C('MlpPolicy',
-    #                 victim,
-    #                 verbose=0,
-    #                 gamma=0.99)
-    #
-    # A2C_model.learn(total_timesteps=1000000,
-    #                 log_interval=100)
-    #
-    # A2C_model.save("models/a2c_all_arms_action_reward3")
-    #
-    # A2C_model = A2C.load("models/a2c_all_arms_action_reward3")
-    #
-    # a2c_victim_reward, a2c_victim_cum_regret, a2c_attacker_reward = test_attacker(A2C_model, victim)
-    #
-    # plt.plot(a2c_victim_cum_regret)
-    # plt.show()
-    # plt.plot(a2c_attacker_reward)
-    # plt.show()
+    ## A2C
+    A2C_model = A2C('MlpPolicy',
+                    victim,
+                    verbose=1,
+                    gamma=0.99)
+    A2C_model.learn(total_timesteps=1000000,
+                    log_interval=100)
+    A2C_model.save("models/a2c_chosen_arm_action_reward")
+    A2C_model = A2C.load("models/a2c_chosen_arm_action_reward")
+    a2c_victim_reward, a2c_victim_cum_regret, a2c_attacker_reward = test_attacker(A2C_model, victim)
 
-    # DDPG_model = DDPG('MlpPolicy',
-    #                 victim,
-    #                 verbose=0,
-    #                 gamma=0.99)
-    #
-    # DDPG_model.learn(total_timesteps=100000,
-    #                 log_interval=100)
-    #
-    # DDPG_model.save("models/ddpg_all_arms_action_reward3")
-
-    DDPG_model = DDPG.load("models/ddpg_all_arms_action_reward3")
-
+    ## DDPG
+    DDPG_model = DDPG('MlpPolicy',
+                    victim,
+                    verbose=1,
+                    gamma=0.99)
+    DDPG_model.learn(total_timesteps=1000000,
+                    log_interval=100)
+    DDPG_model.save("models/ddpg_chosen_arm_action_reward")
+    DDPG_model = DDPG.load("models/ddpg_chosen_arm_action_reward")
     ddpg_victim_reward, ddpg_victim_cum_regret, ddpg_attacker_reward = test_attacker(DDPG_model, victim)
 
-    plt.plot(ddpg_victim_cum_regret)
-    plt.show()
-    plt.plot(ddpg_attacker_reward)
-    plt.show()
 
+    ## PLOTTING
+    ## Regret plots
+    fig = plt.figure(figsize=(10,6))
+    plt.plot(vanilla_victim_cum_regret, color=palette.DEFAULT_COLORS[0])
+    plt.plot(random_attack_victim_cum_regret, color=palette.DEFAULT_COLORS[1])
+    plt.plot(ddpg_victim_cum_regret, color=palette.DEFAULT_COLORS[2])
+    plt.plot(a2c_victim_cum_regret, color=palette.DEFAULT_COLORS[3])
+    plt.plot(ppo_victim_cum_regret, color=palette.DEFAULT_COLORS[4])
+    plt.plot(sac_victim_cum_regret, color=palette.DEFAULT_COLORS[5])
+    plt.xlabel('iterations')
+    plt.ylabel('regret')
+    plt.title('Victim-Level Cumulative Regret')
+    plt.grid()
+    plt.legend(('No attack', 'Uniform random', 'DDPG', 'A2C', 'PPO', 'SAC'))
+    plt.show()
 
 
 if __name__ == '__main__':
